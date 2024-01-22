5fd17c1840c2ef0111d3de8f41ce8f17ddd3dea4
Shayan <sh0davoodi@gmail.com>
2024-01-23 00:46:37 +0330

    [cepstral.jl] Important fixes 🔧

--- src/cepstral.jl
+++ src/cepstral.jl
@@ -1,20 +1,22 @@
 function cepscoef(p::Int, α::AbstractVector, n::Int)
   n>0 || ArgumentError("n must be positive") |> throw
   c = similar(α, n)
-  if n==1
-    c[1] = -α[1]
-  elseif 1<n≤p
-    secondterm = 0.
-    for m ∈ 1:n-1
-      secondterm += (1-m/n)*α[m]*c[n-m]
+  for n_ ∈ 1:n
+    if n_==1
+      c[1] = -α[1]
+    elseif 1<n_≤p
+      secondterm = 0.
+      for m ∈ 1:n_-1
+        secondterm += (1-m/n_)*α[m]*c[n_-m]
+      end
+      c[n_] = -α[n_] - secondterm
+    else
+      res = 0.
+      for m ∈ 1:p
+        res += (1-m/n_)*α[m]*c[n_-m]
+      end
+      c[n_] = -res
     end
-    c[n] = -α[n] - secondterm
-  else
-    res = 0.
-    for m ∈ 1:p
-      res += (1-m/n)*α[m]*c[n-m]
-    end
-    c[n] = -res
   end
   return c
 end
