bc8f164799b30a443050be0be27f1fbfe679330f
Shayan <sh0davoodi@gmail.com>
2024-01-23 13:47:23 +0330

    [CepstralClustering.jl] Code refactoring and normalizer adapting 🔧

--- src/CepstralClustering.jl
+++ src/CepstralClustering.jl
@@ -1,39 +1,48 @@
 module CepstralClustering
 
 using ARCHModels
+using Statistics
 
 include("arima.jl")
 include("cepstral.jl")
+include("normalizer.jl")
 
 export cc
 
 """
-    cc(series::AbstractMatrix, p::Int, n::Int)
+    cc(tseries::AbstractMatrix, p::Int, n::Int)
 
 Calculate the cepstral coefficients of the AR(p) process for each asset in the series.
 
 # Arguments
-- `series::AbstractMatrix`: a matrix of time series observations, with each row representing \
+- `tseries::AbstractMatrix`: a matrix of time series observations, with each row representing \
 an asset and each column representing a time step.
 - `p::Int`: the order of the AR(p) process.
 - `n::Int`: the number of cepstral coefficients to calculate.
 
+## Keyword Arguments
+- `normalize::Bool=false`: whether to normalize the series before fitting the AR(p) process.
+
 # Returns
-- `cc_vec::AbstractMatrix`: a matrix of cepstral coefficients, with each row representing an \
+- `cc_mat::AbstractMatrix`: a matrix of cepstral coefficients, with each row representing an \
 asset and each column representing a cepstral coefficient.
 """
 function cc(
-  series::AbstractMatrix,
+  tseries::AbstractMatrix,
   p::Int,
-  n::Int
+  n::Int;
+  normalize::Bool=false
 )
-  n_assets = size(series, 1)
-  cc_vec = similar(series, n_assets, n)
+  tseries = permutedims(tseries)
+  series = copy(tseries)
+  n_assets = size(series, 2)
+  cc_mat = similar(series, n, n_assets)
+  normalize && normalizer!(series)
   α = fit_arima(series, p)
   for asset ∈ 1:n_assets
-    cc_vec[asset, :] = cepscoef(p, α[asset, :], n)
+    cc_mat[:, asset] = cepscoef(p, α[:, asset], n)
   end
-  return cc_vec
+  return cc_mat
 end
 
 end #module
