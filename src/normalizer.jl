92e366717fca9441fd9edc95d9cdabdc64a2b219
Shayan <sh0davoodi@gmail.com>
2024-01-27 18:32:19 +0330

    [normalizer.jl] Redundant method got deleted 🧹

--- src/normalizer.jl
+++ src/normalizer.jl
@@ -1,9 +1,1 @@
-function normalizer!(series::AbstractMatrix)
-  n_obs, n_assets = size(series)
-  for asset ∈ 1:n_assets
-    normalizer!(@view series[:, asset])
-  end
-  return series
-end
-
 normalizer!(series::AbstractVector) = series .= (series .- mean(series)) ./ std(series)
