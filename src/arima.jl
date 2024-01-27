7d88852ca48149f2697c7c2370e768529df83c1e
Shayan <sh0davoodi@gmail.com>
2024-01-27 18:35:22 +0330

    [arima.jl] desc 📃 + Redundant method got removed 🧹 + Type assertion added ➕

--- src/arima.jl
+++ src/arima.jl
@@ -1,14 +1,4 @@
-function fit_arima(series::AbstractVector, p::Int)
+function fit_arima(series::AbstractVector{T}, p::Integer) where T
   model = fit(ARMA{p, 0}, series)
-  return model.meanspec.coefs[2:end]
-end
-
-function fit_arima(series::AbstractMatrix, p::Int)
-  p>0 || ArgumentError("p must be positive") |> throw
-  n_obs, n_assets = size(series)
-  coefs           = similar(series, p, n_assets)
-  for asset ∈ 1:n_assets
-    coefs[:, asset] = fit_arima(series[:, asset], p)
-  end
-  return coefs
+  return model.meanspec.coefs[2:end]::Vector{T}
 end
