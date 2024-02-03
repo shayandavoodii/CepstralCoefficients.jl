85c54fcc80a52e63a6c7022888017b41e99239d7
Shayan <sh0davoodi@gmail.com>
2024-02-03 11:31:36 +0330

    [cepstral.jl] Code refactoring, support ARMACepstral 🔥

--- src/cepstral.jl
+++ src/cepstral.jl
@@ -1,16 +1,25 @@
 abstract type CepstralCoeffModel end
 
-struct ARCepstral <: CepstralCoeffModel end
+struct ARCepstral <: CepstralCoeffModel
+  p::Integer
+end
 
 struct RealCepstral <: CepstralCoeffModel end
 
-function cepscoef(::Type{ARCepstral}, α::AbstractVector, p::Integer, n::Int)
+struct ARMACepstral{S<:AbstractFloat, T<:Integer} <: CepstralCoeffModel
+  σ²::S
+  order::Tuple{T, T}
+end
+
+function cepscoef(m::ARCepstral, tseries::AbstractVector, n::Integer)
   n>0 || ArgumentError("n must be positive") |> throw
+  α = fit_arima(tseries, m.p)
+  m.p == length(α) || ArgumentError("Length of coefficients must be equal to p") |> throw
   c = similar(α, n)
   for n_ ∈ 1:n
     if n_==1
       c[1] = -α[1]
-    elseif 1<n_≤p
+    elseif 1<n_≤m.p
       secondterm = 0.
       for m ∈ 1:n_-1
         secondterm += (1-m/n_)*α[m]*c[n_-m]
@@ -18,7 +27,7 @@ function cepscoef(::Type{ARCepstral}, α::AbstractVector, p::Integer, n::Int)
       c[n_] = -α[n_] - secondterm
     else
       res = 0.
-      for m ∈ 1:p
+      for m ∈ 1:m.p
         res += (1-m/n_)*α[m]*c[n_-m]
       end
       c[n_] = -res
@@ -27,9 +36,68 @@ function cepscoef(::Type{ARCepstral}, α::AbstractVector, p::Integer, n::Int)
   return c
 end
 
-function cepscoef(::Type{RealCepstral}, series::AbstractVector, p::Integer, n::Int)
-  p≥n || ArgumentError("`p` must be equal to or greater than `n` when using `RealCepstral`. \
-  Passed $p and $n.") |> throw
-  res = series |> fft .|> abs .|> log |> ifft
-  return res[1:n]
+function cepscoef(::RealCepstral, tseries::AbstractVector, n::Integer)
+  res = tseries |> fft .|> abs .|> log |> ifft
+  return real.(res[1:n])
+end
+
+"""
+    ψfunc(
+      coefs::AbstractArray,
+      σ²::AbstractFloat,
+      freq::AbstractFloat,
+      order::NTuple{3, Int},
+      n::Int
+    )
+
+Compute the first n cepstral coefficients for each asset and each time period
+
+# Arguments
+- `α`: Vector of coefficients of the ARMA(p, q) model.
+- `σ²`: Variance of the white noise process.
+- `order`: Order of the ARMA(p, q) model.
+- `n`: Number of cepstral coefficients to compute.
+
+# Returns
+- `ψarr::AbstractArray`: Array of cepstral coefficients. First index: coefficient, \
+second index: asset, third index: time window
+"""
+function cepscoef(
+  m::ARMACepstral,
+  tseries::AbstractVector,
+  n::Integer
+)
+  p, q = first(m.order), last(m.order)
+  α = fit_arima(tseries, p, q)
+  ψarr = similar(α, n)
+  for k ∈ 1:n
+    ψarr[k] = quadgk(ω->ψfunc(α, m.σ², ω, p, q, k-1), 0, 1) |> first
+  end
+  return ψarr
+end
+
+function ψfunc(
+  coefs::AbstractVector,
+  σ²::AbstractFloat,
+  freq::AbstractFloat,
+  p::Integer,
+  q::Integer,
+  k::Integer
+)
+  p+q == length(coefs) || ArgumentError("Length of coefficients must be equal to p + q") |> throw
+  a = σ²/2π
+  numerator_ = 0.
+  denominator_ = 0.
+  for h ∈ 1:p
+    x = h * freq
+    numerator_ += coefs[h] * cis(x)
+  end
+  for h ∈ 1:q
+    x = h * freq
+    denominator_ += coefs[p + h] * cis(x)
+  end
+  numerator_ = 1 - numerator_
+  denominator_ = 1 - denominator_
+  λxw = (a*abs(numerator_/denominator_) |> log) * cos(2π*k*freq)
+  return λxw
 end
