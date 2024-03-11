243a88a79f941d0b4a389b660ea5f1f2799596ca
Shayan <sh0davoodi@gmail.com>
2024-03-12 01:49:29 +0330

    [tools.jl] `normalizer!` removed 🧹

--- src/tools.jl
+++ src/tools.jl
@@ -1,4 +1,3 @@
-normalizer!(series::AbstractVector) = series .= (series .- mean(series)) ./ std(series)
 normalizer(series::AbstractVector) = (series .- mean(series)) ./ std(series)
 
 mean(series::AbstractVector) = sum(series) / length(series)
