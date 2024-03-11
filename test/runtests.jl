7115d2fb33eb8e55d82523e8c8924b88c0351f3e
Shayan <sh0davoodi@gmail.com>
2024-03-12 01:47:26 +0330

    [runtests.jl] clustering unit tests included 🔬

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,4 +4,6 @@ using Test
 @testset "CepstralCoefficients.jl" begin
   @info "Testing cepstral.jl unit tests"
   include("cepstral.jl")
+  @info "Testing clustering.jl unit tests"
+  include("clustering.jl")
 end
