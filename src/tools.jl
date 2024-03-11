ed8f48c2b519491659c845d7c0736568f609bc73
Shayan <sh0davoodi@gmail.com>
2024-03-11 20:48:16 +0330

    [tools.jl] `normalizer` added 🆕

--- src/tools.jl
+++ src/tools.jl
@@ -1,3 +1,4 @@
 normalizer!(series::AbstractVector) = series .= (series .- mean(series)) ./ std(series)
+normalizer(series::AbstractVector) = (series .- mean(series)) ./ std(series)
 
-@inline mean(series::AbstractVector) = sum(series) / length(series)
+mean(series::AbstractVector) = sum(series) / length(series)
