2cbaa0a486baea03f121bd72c31cc6e721809587
Shayan <sh0davoodi@gmail.com>
2024-03-14 16:56:55 +0330

    [clustering.jl] Docstring added 📄

--- src/clustering.jl
+++ src/clustering.jl
@@ -1,1 +1,17 @@
+"""
+    cepsclustering(cc::AbstractMatrix, k::Integer)
+
+Perform clustering of cepstral coefficients using k-medoids algorithm.
+
+!!! note
+    This function can be used only after importing the `Clustering` package.
+
+# Arguments
+- `cc::AbstractMatrix`: A matrix of cepstral coefficients.
+- `k::Integer`: Maximum number of clusters to be examined (to find the optimal number of clusters).
+
+# Returns
+- A vector of vectors, where each vector contains the indices of the cepstral coefficients in \
+  the corresponding cluster.
+"""
 function cepsclustering end
